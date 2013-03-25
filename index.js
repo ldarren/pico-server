2529cbbe7cb87d96eb53dafb2bd36b05567dc4ca
Darren Liew <ldarren@gmail.com>
2013-03-25 12:58:44 +0000

    added ipc lib, modifed index.js for clustering

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-//process.on('uncaughtException', function(err){
-//    console.error('[uncaught exception', err);
-//});
+process.on('uncaughtException', function(err){
+    console.error('[uncaught exception', err);
+});
 
-require('../common/const');
+require('./lib/const');
 
 var
-path = require('path'),
-loadConfig = require('./lib/utils').loadConfig,
-args = process.argv,
-env = process.env,
-root,
-i=1,
-l=args.length;
+cluster = require('cluster'),
+loadConfig = require('./lib/utils').loadConfig;
 
-// find root path
-for(; i<l; i++){ if (args[i].charAt(1) !== '-') break; }
+exports.createApp = function(rootPath, configBasePath, configPath){
+    var env = process.env;
 
-root = path.dirname(args[i]);
-console.log('root:',root);
-for(; i<l; i++){
-    switch(args[i]){
-        case '-h':
-            console.log('usage help here');
-            break;
-        case '-c':
-            var
-            c = root + '/' + args[++i],
-            p = path.dirname(c);
-            loadConfig(root, require(p+'/base'), require(c));
-            break;
-    }
-}
+    if (env.picoConfigBasePath) configBasePath = env.picoConfigBasePath;
+    if (env.picoConfigPath) configPath = env.picoConfigPath;
+
+    loadConfig(rootPath, configBasePath, configPath, function(err, config){
+        if (err) return console.error(err);
+
+        if (cluster.isMaster){
+            if (!config.worker) return console.error('no worker config found');
+            var workerCfg = config.worker;
+
+            cluster.setupMaster({
+                exec: rootPath
+            });
+
+            for(var i=0,l=workerCfg.count || require('os').cpus().length; i<l; i++){
+                cluster.fork({
+                    picoConfigBasePath: workerCfg.cfgBase,
+                    picoConfigPath:workerCfg.cfg
+                });
+            }
+        }
+    });
+});
