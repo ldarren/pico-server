a965ff195f2506c99671e26f17781a138d7ce864
Darren Liew <ldarren@gmail.com>
2013-04-13 16:00:34 +0000

    added zip and unzip to pico utils, moved createApp to pico utils from index.js

--- index.js
+++ index.js
@@ -2,41 +2,11 @@ require('pico-common/const');
 require('./lib/const');
 
 var
-path = require('path'),
-cluster = require('cluster'),
-loadConfig = require('./lib/utils').loadConfig;
+utils = require('./lib/utils'),
+apiList = utils.apiList,
+api;
 
-exports.createApp = function(rootPath, configBasePath, configPath){
-    var env = process.env;
-
-    if (env.picoConfigBasePath) configBasePath = env.picoConfigBasePath;
-    if (env.picoConfigPath) configPath = env.picoConfigPath;
-console.log(rootPath,configBasePath,configPath);
-    loadConfig(rootPath, configBasePath, configPath, function(err, config){
-        if (err) return console.error(err);
-
-        if ('live' === config.app.env){
-            // this might supress bug alert
-            process.on('uncaughtException', function(err){
-                console.error('[uncaught exception', err);
-            });
-        }
-
-
-        if (cluster.isMaster){
-            if (!config.worker) return console.error('no worker config found');
-            var workerCfg = config.worker;
-
-            cluster.setupMaster({
-                exec: rootPath
-            });
-
-            for(var i=0,l=workerCfg.count || require('os').cpus().length; i<l; i++){
-                cluster.fork({
-                    picoConfigBasePath: rootPath + path.sep + workerCfg.cfgBase,
-                    picoConfigPath: rootPath + path.sep + workerCfg.cfg
-                });
-            }
-        }
-    });
-};
+for (var i=0,l=apiList.length; i<l; i++){
+    api = apiList[i];
+    exports[api] = utils[api];
+}
