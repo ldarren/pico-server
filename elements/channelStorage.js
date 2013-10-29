d05db77c6af5fc607f5a72754fd0bd3460823afa
Darren Liew <ldarren@gmail.com>
2013-10-29 11:57:47 +0000

    added channel pass field

--- elements/channelStorage.js
+++ elements/channelStorage.js
@@ -1,9 +1,13 @@
 const
 KEY_CHANNEL = 'hC:',
 KEY_CHANNEL_COUNTER = 'sCC:',
-KEY_MESSAGE_COUNTER = 'counter';
+KEY_MESSAGE_COUNTER = 'counter',
+KEY_MESSAGE_PASS = 'pass';
 
-var ChannelStorage = module.exports = {
+var
+Random = Math.random,
+Ceil = Math.ceil,
+ChannelStorage = module.exports = {
     client: null,
     expiry: 1 * 60 * 60,  // expire after 1 hour
 };
@@ -17,7 +21,9 @@ function getChannelCounterKey(min){
 function getMessageCounterKey(channelId){
     return KEY_MESSAGE_COUNTER + channelId;
 }
-
+function newMessagePass(client, cb){
+    return Ceil(Random()*1000);
+}
 ChannelStorage.prototype = {
     setup: function(redisClient, expiry){
         this.client = redisClient;
@@ -25,25 +31,60 @@ ChannelStorage.prototype = {
     },
     newChannelId: function(cb){
         var
+        client = this.client,
+        expiry = this.expiry,
         now = new Date(),
         key = getChannelCounterKey(now.getMinutes());
-        this.client.multi()
+
+        client.multi()
         .incr(key)
         .expire(key, 120)
         .exec(function(err, ret){
-            cb(err, ret ? now.getTime()*1000 + ret[0] : undefined);
+            if (err) return cb(err);
+            var
+            channelId = now.getTime()*1000 + ret[0],
+            channelPass = newMessagePass();
+
+            key = getChannelKey(channelId);
+
+            client.multi()
+            .hset(key, KEY_MESSAGE_PASS, channelPass)
+            .expire(key, expiry)
+            .exec(function(err){
+                return cb(err, channelId, channelPass);
+            });
+        });
+    },
+    renewChannelId: function(channelId, oldChannelPass, cb){
+        var
+        client = this.client,
+        expiry = this.expiry,
+        key = getChannelKey(channelId);
+
+        client.hget(key, KEY_MESSAGE_PASS, function(err, pass){
+            if (err) return cb(err);
+            if (oldChannelPass != pass) return cb('wrong channel pass');
+
+            var channelPass = newMessagePass();
+
+            client.multi()
+            .hset(key, KEY_MESSAGE_PASS, channelPass)
+            .expire(key, expiry)
+            .exec(function(err){
+                return cb(err, channelId, channelPass);
+            });
         });
     },
     newMessageId: function(channelId, cb){
-        var cid = getChannelKey(channelId);
-        this.client.hincrby(cid, KEY_MESSAGE_COUNTER, 1, cb);
+        var key = getChannelKey(channelId);
+        this.client.hincrby(key, KEY_MESSAGE_COUNTER, 1, cb);
     },
 
     storeMessage: function(channelId, msgId, msg, cb){
-        var cId = getChannelKey(channelId);
+        var key = getChannelKey(channelId);
         this.client.multi()
-        .hset(cId, msgId, msg)
-        .expire(cId, this.expiry)
+        .hset(key, msgId, msg)
+        .expire(key, this.expiry)
         .exec(function(err){
             return cb(err);
         });
@@ -52,6 +93,8 @@ ChannelStorage.prototype = {
         this.client.hkeys(getChannelKey(channelId), function(err, msgIds){
             if (err) return cb(err);
             var i = msgIds.indexOf(KEY_MESSAGE_COUNTER);
+            msgIds.splice(i, 1);
+            i = msgIds.indexOf(KEY_MESSAGE_PASS);
             msgIds.splice(i, 1);
             cb(null, msgIds);
         });
