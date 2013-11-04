7fff5e3d0276106b0d0d2b501a142e923613ad93
Darren Liew <ldarren@gmail.com>
2013-11-04 08:25:04 +0000

    fixed exception if modelid or key not sound in session

--- samples/webchat/actions/index.js
+++ samples/webchat/actions/index.js
@@ -8,7 +8,7 @@ testResponse = function(session, order, next){
         undefined,
         undefined,
         G_PICO_WEB.RENDER_FULL,
-        [[session.createModelInfo('foobar', 'me')]]
+        [[session.createModelInfo('foobar1', 'me2')]]
     );
 
     next();
