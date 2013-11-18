2a5f912729427201459f44a948474f179d2437eb
Darren Liew <ldarren@gmail.com>
2013-11-18 12:07:58 +0000

    implemented universal analytics

--- samples/webchat/actions/index.js
+++ samples/webchat/actions/index.js
@@ -1,5 +1,6 @@
 var
 web = null,
+analytics = null,
 testResponse = function(session, order, next){
     var model = session.getModel('foobar');
     model['me'] = 'world';
@@ -11,11 +12,13 @@ testResponse = function(session, order, next){
         [[session.createModelInfo('foobar', 'me')]]
     );
 
+    analytics.pageview('107.20.154.29','test','testAPI').event('test', 'foobar');
     next();
 },
 router = {
     setup: function(context, next){
         web = context.webServer;
+        analytics = context.ritbAnalytics;
 
         web.setChannelStorage(require('../models/channelStorage'));
         
