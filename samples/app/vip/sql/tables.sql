f033dc00689639759f147e97f0dc58669bbb8079
Darren Liew <ldarren@gmail.com>
2014-06-02 15:49:30 +0000

    updated table

--- samples/app/vip/sql/tables.sql
+++ samples/app/vip/sql/tables.sql
@@ -2,13 +2,75 @@ CREATE DATABASE IF NOT EXISTS `vip`;
 
 USE `vip`;
 
-CREATE TABLE IF NOT EXISTS `shop`(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(64) NOT NULL,
-    message VARCHAR(1024),
-    createdBy BIGINT NOT NULL,
-    createdAt DATETIME NOT NULL,
-    updatedBy BIGINT,
-    updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS `user`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(64) NOT NULL,
+    `username` VARCHAR(64) NOT NULL,
+    `password` VARCHAR(64) NOT NULL,
+    `updatedBy` BIGINT,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `createdBy` BIGINT NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
+)
+
+CREATE TABLE IF NOT EXISTS `group`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(64) NOT NULL,
+    `message` VARCHAR(160),
+    `logo` VARCHAR(64),
+    `status` BIT(1) DEFAULT 1,
+    `updatedBy` BIGINT,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `createdBy` BIGINT NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
+)
+
+CREATE TABLE IF NOT EXISTS `follow`(
+    `userId` BIGINT NOT NULL,
+    `groupId` BIGINT NOT NULL,
+    `status` BIT(1) DEFAULT 1,
+    `updatedBy` BIGINT,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `createdBy` BIGINT NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    PRIMARY KEY (`userId`,`groupId`)
+)
+
+CREATE TABLE IF NOT EXISTS `tag`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(64) NOT NULL,
+    `status` BIT(1) DEFAULT 1,
+    `updatedBy` BIGINT,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `createdBy` BIGINT NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY (`name`)
+)
+
+CREATE TABLE IF NOT EXISTS `groupTag`(
+    `groupId` BIGINT NOT NULL,
+    `tagId` BIGINT NOT NULL,
+    `status` BIT(1) DEFAULT 1,
+    `updatedBy` BIGINT,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `createdBy` BIGINT NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    PRIMARY KEY (`groupId`, `tagId`)
+)
+
+CREATE TABLE IF NOT EXISTS `flyer`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `page` TINYINT(8) DEFAULT 0,
+    `groupId` BIGINT NOT NULL,
+    `startAt` DATETIME,
+    `endAt` DATETIME,
+    `status` BIT(1) DEFAULT 1,
+    `updatedBy` BIGINT,
+    `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `createdBy` BIGINT NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
 )
