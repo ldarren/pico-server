e5999d9252f325ef1e170b0ba2151069d4d217ac
Darren Liew <ldarren@gmail.com>
2014-06-12 16:49:23 +0000

    better error checking

--- samples/app/vip/actions/user.js
+++ samples/app/vip/actions/user.js
@@ -6,7 +6,7 @@ var sql = require('../models/sql/user');
 
 module.exports = {
     read: function(session, order, next){
-        sql.read(order.username, function(err, result){
+        sql.read(order.email, function(err, result){
             if (err) return next(err);
 
             var model = session.getModel(MODEL);
@@ -18,5 +18,16 @@ module.exports = {
 
             next();
         });
+    },
+    create: function(session, order, next){
+
+            var model = session.getModel(MODEL);
+            model[ME] = {hello:'world'};
+            session.addJob(
+                G_PICO_WEB.RENDER_FULL,
+                [[session.createModelInfo(MODEL, ME)]]
+            );
+
+            next();
     },
 };
